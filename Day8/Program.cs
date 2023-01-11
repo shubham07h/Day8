aa3dbb0e533c8c43aef4e0d991fca8a3c911ed01
shubham07h <shubham1609sharma@gmail.com>
2023-01-11 16:49:23 +0530

    commit

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace lineComparision
+{
+    public class Program
+    {
+        public static void main(double x1, double x2, double y1, double y2)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            double sum, length;
+            sum = ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            length = Math.Sqrt(sum);
+            Console.WriteLine("length of Line - " + length);
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1st coordinates ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter 2nd coordinates ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            Program.main(x1, y1, x2, y2);
+        }
+    }
+}
