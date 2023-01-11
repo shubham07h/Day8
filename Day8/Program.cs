aece2bf060bcfcac07ddf5dd105ea15f0c525f1a
shubham07h <shubham1609sharma@gmail.com>
2023-01-11 16:58:19 +0530

    commit

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -8,23 +8,46 @@ namespace lineComparision
             double length1, length2;
             if (x1 == x2)//start point equal
             {
-                Console.WriteLine(y1.Equals(y2));//check end points are equal
+                double compare = y1.CompareTo(y2);
+                if (compare == 0)
+                {
+                    Console.WriteLine("Both Lines are Equals");
+                }
+                else if (compare >= 0)
+                {
+                    Console.WriteLine("2nd line is smaller than 1 st");
+                }
+                else
+                {
+                    Console.WriteLine("1st line is smaller than 2nd");
+                }
             }
             else
             {
                 length1 = x1 - x2;
                 length2 = y1 - y2;
-                Console.WriteLine(length1.Equals(length2));
-                Console.ReadLine();
+                double compare = length1.CompareTo(length2);
+                if (compare == 0)
+                {
+                    Console.WriteLine("Both Lines are Equals");
+                }
+                else if (compare >= 0)
+                {
+                    Console.WriteLine("2nd line is smaller than 1 st");
+                }
+                else
+                {
+                    Console.WriteLine("1st line is smaller than 2nd");
+                }
             }
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("To check Both lines are Equals or no using equals method");
-            Console.WriteLine("Enter start and end point of 1 st line ");
+            Console.WriteLine("To Compare Both Line using CompareTo method");
+            Console.WriteLine("Enter 1 st line coordinates ");
             double x1 = Convert.ToDouble(Console.ReadLine());
             double y1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter start and end point of 2 nd line ");
+            Console.WriteLine("Enter 2 nd line coordinates ");
             double x2 = Convert.ToDouble(Console.ReadLine());
             double y2 = Convert.ToDouble(Console.ReadLine());
             Program.main(x1, y1, x2, y2);
